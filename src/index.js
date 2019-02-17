6dc853c00cbfe6d14b2742d83c408d434b36d728
Greg Westneat <greg.westneat@gmail.com>
2019-02-16 19:40:43 -0800

    Inish

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Dank!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
 `;
